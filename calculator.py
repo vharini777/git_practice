040c00b9b2a79acb1335a6bd9cf24f7cb2a1a872
V Harini <vharini.ece2024@citchennai.net>
2026-06-10 14:10:58 +0530

    calculator edited

--- calculator.py
+++ calculator.py
@@ -1,12 +1,24 @@
-def add(a ,b):
-	return a+b
-def sub(a , b):
-	return a-b
-def mul(a , b):
-	return a*b
-def div(a , b):
-	if(b !=0):
-		return a/b
-	else:
-		raise ValueError("Division by 0 not possible")
+def add(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
 
+
+def sub(a, b):
+    """Returns the difference between two numbers."""
+    return a - b
+
+
+def mul(a, b):
+    """Returns the product of two numbers."""
+    return a * b
+
+
+def div(a, b):
+    """
+    Returns the quotient of two numbers.
+    Raises ValueError if denominator is zero.
+    """
+    if b != 0:
+        return a / b
+    else:
+        raise ValueError("Division by 0 not possible")
